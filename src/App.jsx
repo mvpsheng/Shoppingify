5713269204f62613911bb6c422fcfc6f71697e9a
mvpsheng <51054870+mvpsheng@users.noreply.github.com>
2023-06-21 01:40:56 +0000

    Add Zoom structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <main>
       <h1>shoppingify</h1>
+      <div>
+        <div>Tabs</div>
+        <div>Mainbody</div>
+        <div>Shopping List</div>
+      </div>
     </main>
   )
 }
---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+export default function Item() {
+  return (
+    <main>
+      <h1>Item</h1>
+    </main>
+  )
+}
